fe2bc12711bf04f1c66105ddc97d508f83b66674
Shantanu <joshishantanu4@gmail.com>
2025-01-03 15:09:29 -0800

    chrome-extension: set host_permissions for extension & content script

--- chrome-extension/manifest.js
+++ chrome-extension/manifest.js
@@ -36,10 +36,10 @@ const manifest = withSidePanel({
    * if you want to support multiple languages, you can use the following reference
    * https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Internationalization
    */
-  name: '__MSG_extensionName__',
+  name: 'Savvy',
   version: packageJson.version,
-  description: '__MSG_extensionDescription__',
-  host_permissions: ['<all_urls>'],
+  description: 'Create, Share, and Run Worfklows from your browser & CLI',
+  host_permissions: ['https://app.getsavvy.so/*', 'http://localhost:5173'],
   permissions: ['storage', 'scripting', 'tabs', 'notifications'],
   options_page: 'options/index.html',
   background: {
@@ -58,16 +58,8 @@ const manifest = withSidePanel({
   },
   content_scripts: [
     {
-      matches: ['http://*/*', 'https://*/*', '<all_urls>'],
+      matches: ['http://app.getsavvy.so/*', 'https://localhost:5173/*'],
       js: ['content/index.iife.js'],
-    },
-    {
-      matches: ['http://*/*', 'https://*/*', '<all_urls>'],
-      js: ['content-ui/index.iife.js'],
-    },
-    {
-      matches: ['http://*/*', 'https://*/*', '<all_urls>'],
-      css: ['content.css'], // public folder
     },
   ],
   devtools_page: 'devtools/index.html',
