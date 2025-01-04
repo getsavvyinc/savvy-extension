90b679c880403f9f2dd319064bb2e4233c01f567
Shantanu <joshishantanu4@gmail.com>
2025-01-03 18:34:35 -0800

    remove content-ui test

--- tests/e2e/specs/page-content-ui.test.ts
+++ tests/e2e/specs/page-content-ui.test.ts
@@ -1,8 +1,9 @@
-describe('Content UI Injection', () => {
-  it('should locate the injected content UI div', async () => {
-    await browser.url('https://www.example.com');
-
-    const contentDiv = await $('#chrome-extension-boilerplate-react-vite-content-view-root').getElement();
-    await expect(contentDiv).toBeDisplayed();
-  });
-});
+// We don't have any content UI to test yet, so this test is commented out.
+//describe('Content UI Injection', () => {
+//  it('should locate the injected content UI div', async () => {
+//    await browser.url('https://www.example.com');
+//
+//    const contentDiv = await $('#chrome-extension-boilerplate-react-vite-content-view-root').getElement();
+//    await expect(contentDiv).toBeDisplayed();
+//  });
+//});
